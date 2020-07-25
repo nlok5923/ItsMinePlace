d59a995331f41a0ad0195bf382e30cfb82f107a7
Creator5923 <nlokhande5923@gmail.com>
2020-07-25 03:29:52 -0700

    added social links

--- src/Components/About.js
+++ src/Components/About.js
@@ -10,6 +10,11 @@ const About =()=>{
             <img className="proImage" src="Images/demo.jpg" alt="earth Image"></img>
             <h1 className="proName">Nitanshu Lokhande</h1>
             <p>&emsp;&emsp;&emsp;&emsp;Student at IIIT Vadodara</p>
+<br /><br />
+<a href="https://www.facebook.com/nitanshu.lokhande.9/" class="fa fa-facebook"></a>
+<a href="https://www.instagram.com/nit_lok/" class="fa fa-instagram"></a>
+<a href="https://github.com/nlok5923" class="fa fa-github"></a>
+<a href="https://www.linkedin.com/in/nitanshu-lokhande-5b60691a0/" class="fa fa-linkedin"></a>
         </div>
     );
   
