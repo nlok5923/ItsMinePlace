39787fb46050de7b8ff468e788df85ce6295fd01
Creator5923 <nlokhande5923@gmail.com>
2020-11-01 08:53:01 -0800

    working on project section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,19 +4,39 @@ class Projects extends Component {
     render() {
         return (
             <div className="project__section">
-                <h1> Mine Projects </h1>
+                <h1 className="proj__head"> Mine Projects </h1>
                 <div className ="projects">
                 <div className="project">
                     <img src="./Images/lightyear.jpg" alt="proj__img"></img>
+                <div className ="project__head">
+                <h1>This is sample project</h1>
+                <p>This is the sample data for the sample project listed here so that user get to know about it</p>
+
+                </div>
                 </div>
                 <div className="project">
+                <div className ="project__head">
+                <h1>This is sample project</h1>
+                <p>This is the sample data for the sample project listed here so that user get to know about it</p>
+
+                </div>
+                <img src="./Images/lightyear.jpg"  alt="proj__img"></img>
+                </div>
+                <div className="project">
+                <div className ="project__head">
+                <h1>This is sample project</h1>
+                <p>This is the sample data for the sample project listed here so that user get to know about it</p>
+
+                </div>
                 <img src="./Images/lightyear.jpg"  alt="proj__img"></img>
                 </div>
                 <div className="project">
                 <img src="./Images/lightyear.jpg"  alt="proj__img"></img>
+                <div className ="project__head">
+                <h1>This is sample project</h1>
+                <p>This is the sample data for the sample project listed here so that user get to know about it</p>
+
                 </div>
-                <div className="project">
-                <img src="./Images/lightyear.jpg"  alt="proj__img"></img>
                 </div>
                 </div>
             </div>
